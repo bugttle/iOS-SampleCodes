6b9daa7075192f379267c999b335f08b257225f0
UQ Times <uqtimes@gmail.com>
2012-05-05 15:11:22 +0900

    一つの関数にまとめようとしたが、やはり分かりづらいので関数化

--- GestureRecognizers/GestureRecognizers/ViewController.m
+++ GestureRecognizers/GestureRecognizers/ViewController.m
@@ -20,97 +20,22 @@
 @synthesize showGestureName;
 @synthesize showSubInformation;
 
-/* 一つの view に全てのジェスチャーを設定 */
-- (void)setAllGestures {
-    // 一本指でシングルタップ
-    UITapGestureRecognizer *singleFingerSingleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleFingerSingleTap:)];
-    // 一本指でダブルタップ
-    UITapGestureRecognizer *singleFingerDoubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleFingerDoubleTap:)];
-    [singleFingerDoubleTap setNumberOfTapsRequired:2];    
-    // ダブルタップの失敗時のみシングルタップを実行する (シングルタップとダブルタップが同時に認識されてしまうのを回避する)
-    // もちろんシングルタップの動作が鈍くなる
-    [singleFingerSingleTap requireGestureRecognizerToFail:singleFingerDoubleTap];
-    [self.allGesturesView addGestureRecognizer:singleFingerSingleTap];
-    [singleFingerSingleTap release];
-    [self.allGesturesView addGestureRecognizer:singleFingerDoubleTap];
-    [singleFingerDoubleTap release];
-    
-    // 二本指でシングルタップ
-    UITapGestureRecognizer *twoFingerSingleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTwoFingerSingleTap:)];
-    [twoFingerSingleTap setNumberOfTouchesRequired:2];    
-    // 二本指でダブルタップ
-    UITapGestureRecognizer *twoFingerDoubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTwoFingerDoubleTap:)];
-    [twoFingerDoubleTap setNumberOfTouchesRequired:2];
-    [twoFingerDoubleTap setNumberOfTapsRequired:2];
-    // ダブルタップの失敗時のみシングルタップを実行する (シングルタップとダブルタップが同時に認識されてしまうのを回避する)
-    // もちろんシングルタップの動作が鈍くなる
-    [twoFingerSingleTap requireGestureRecognizerToFail:twoFingerDoubleTap];
-    [self.allGesturesView addGestureRecognizer:twoFingerSingleTap];
-    [twoFingerSingleTap release];
-    [self.allGesturesView addGestureRecognizer:twoFingerDoubleTap];
-    [twoFingerDoubleTap release];
-    
-    // ピンチインとピンチアウト
-    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinchGesture:)];
-    [self.allGesturesView addGestureRecognizer:pinchGesture];
-    [pinchGesture release];
-    
-    // ドラッグ
-    [self setPanGesture:allGesturesView];
-    
-    // スワイプ
-    [self setSwipeGesture:allGesturesView];
-        
-    // 回転
-    UIRotationGestureRecognizer *rotationGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotationGesture:)];
-    [self.allGesturesView addGestureRecognizer:rotationGesture];
-    [rotationGesture release];
-    
-    // 長押し
-    UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPressGesture:)];
-    [self.allGesturesView addGestureRecognizer:longPressGesture];
-    [longPressGesture release];
-}
-
-/* スワイプと同じ動作なので分ける */
-- (void)setPanGesture:(UIView *)view {
-    // ドラッグ
-    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
-    [self.panGestureView addGestureRecognizer:panGesture];
-    [panGesture release];
-}
-
-/* ドラッグと同じ動作なので分ける */
-- (void)setSwipeGesture:(UIView *)view {
-    // スワイプ
-    //(UISwipeGestureRecognizerDirectionUp|UISwipeGestureRecognizerDirectionRight|UISwipeGestureRecognizerDirectionDown|UISwipeGestureRecognizerDirectionLeft)とできないものか
-    UISwipeGestureRecognizer *swipeUpGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
-    [swipeUpGesture setDirection:UISwipeGestureRecognizerDirectionUp];
-    [view addGestureRecognizer:swipeUpGesture];
-    [swipeUpGesture release];
-
-    UISwipeGestureRecognizer *swipeRightGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
-    [swipeRightGesture setDirection:UISwipeGestureRecognizerDirectionRight];
-    [view addGestureRecognizer:swipeRightGesture];
-    [swipeRightGesture release];
-
-    UISwipeGestureRecognizer *swipeDownGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
-    [swipeDownGesture setDirection:UISwipeGestureRecognizerDirectionDown];
-    [view addGestureRecognizer:swipeDownGesture];
-    [swipeDownGesture release];
-    
-    UISwipeGestureRecognizer *swipeLeftGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
-    [swipeLeftGesture setDirection:UISwipeGestureRecognizerDirectionLeft];
-    [view addGestureRecognizer:swipeLeftGesture];
-    [swipeLeftGesture release];
-}
-
 - (void)loadView {
     [super loadView];
     
-    [self setAllGestures];
-    [self setPanGesture: panGestureView];
-    [self setSwipeGesture: swipeGestureView];
+    // 全てのジェスチャを設定
+    [self addTagGestureRecognizer:allGesturesView];
+    [self addPinchGestureRecognizer:allGesturesView];
+    [self addPanGestureRecognizer:allGesturesView];
+    [self addSwipeGestureRecognizer:allGesturesView];
+    [self addRotationGestureRecognizer:allGesturesView];
+    [self addLongPressGestureRecognizer:allGesturesView];
+    
+    // スワイプと同じ動作なので、ドラッグジェスチャのみを設定
+    [self addPanGestureRecognizer:panGestureView];
+    
+    // ドラッグと同じ動作なので、スワイプジェスチャのみを設定
+    [self addSwipeGestureRecognizer:swipeGestureView];
 }
 
 - (void)viewDidLoad
@@ -146,8 +71,102 @@
 }
 
 #pragma mark -
+#pragma mark add gestrue recognizer
+
+- (void)addTagGestureRecognizer:(UIView *)view
+{
+    /* 1本指 */
+    // 1本指でシングルタップ
+    UITapGestureRecognizer *singleFingerSingleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleFingerSingleTap:)];
+    // 1本指でダブルタップ
+    UITapGestureRecognizer *singleFingerDoubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleFingerDoubleTap:)];
+    [singleFingerDoubleTap setNumberOfTapsRequired:2];    
+    // ダブルタップの失敗時のみシングルタップを実行する (シングルタップとダブルタップが同時に認識されてしまうのを回避する)
+    // もちろんシングルタップの動作が鈍くなる
+    [singleFingerSingleTap requireGestureRecognizerToFail:singleFingerDoubleTap];
+    [self.allGesturesView addGestureRecognizer:singleFingerSingleTap];
+    [singleFingerSingleTap release];
+    [self.allGesturesView addGestureRecognizer:singleFingerDoubleTap];
+    [singleFingerDoubleTap release];
+    
+    /* 2本指 */
+    // 2本指でシングルタップ
+    UITapGestureRecognizer *twoFingerSingleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTwoFingerSingleTap:)];
+    [twoFingerSingleTap setNumberOfTouchesRequired:2];    
+    // 2本指でダブルタップ
+    UITapGestureRecognizer *twoFingerDoubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTwoFingerDoubleTap:)];
+    [twoFingerDoubleTap setNumberOfTouchesRequired:2];
+    [twoFingerDoubleTap setNumberOfTapsRequired:2];
+    // ダブルタップの失敗時のみシングルタップを実行する (シングルタップとダブルタップが同時に認識されてしまうのを回避する)
+    // もちろんシングルタップの動作が鈍くなる
+    [twoFingerSingleTap requireGestureRecognizerToFail:twoFingerDoubleTap];
+    [self.allGesturesView addGestureRecognizer:twoFingerSingleTap];
+    [twoFingerSingleTap release];
+    [self.allGesturesView addGestureRecognizer:twoFingerDoubleTap];
+    [twoFingerDoubleTap release];
+}
+
+- (void)addPinchGestureRecognizer:(UIView *)view
+{
+    // ピンチインとピンチアウト
+    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinchGesture:)];
+    [self.allGesturesView addGestureRecognizer:pinchGesture];
+    [pinchGesture release];
+}
+
+- (void)addPanGestureRecognizer:(UIView *)view
+{
+    // ドラッグ
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
+    [self.panGestureView addGestureRecognizer:panGesture];
+    [panGesture release];
+}
+
+- (void)addSwipeGestureRecognizer:(UIView *)view
+{
+    // スワイプ
+    // (UISwipeGestureRecognizerDirectionRight|UISwipeGestureRecognizerDirectionLeft|UISwipeGestureRecognizerDirectionUp|UISwipeGestureRecognizerDirectionDown)
+    // とすると、directionでどの方向に動いたかを取得できないため複数のジェスチャを作成
+    UISwipeGestureRecognizer *swipeRightGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
+    [swipeRightGesture setDirection:UISwipeGestureRecognizerDirectionRight];
+    [view addGestureRecognizer:swipeRightGesture];
+    [swipeRightGesture release];
+    
+    UISwipeGestureRecognizer *swipeLeftGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
+    [swipeLeftGesture setDirection:UISwipeGestureRecognizerDirectionLeft];
+    [view addGestureRecognizer:swipeLeftGesture];
+    [swipeLeftGesture release];
+    
+    UISwipeGestureRecognizer *swipeUpGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
+    [swipeUpGesture setDirection:(UISwipeGestureRecognizerDirectionUp)];
+    [view addGestureRecognizer:swipeUpGesture];
+    [swipeUpGesture release];
+    
+    UISwipeGestureRecognizer *swipeDownGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
+    [swipeDownGesture setDirection:UISwipeGestureRecognizerDirectionDown];
+    [view addGestureRecognizer:swipeDownGesture];
+    [swipeDownGesture release];
+}
+
+- (void)addRotationGestureRecognizer:(UIView *)view
+{
+    // 回転
+    UIRotationGestureRecognizer *rotationGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotationGesture:)];
+    [self.allGesturesView addGestureRecognizer:rotationGesture];
+    [rotationGesture release];
+}
+
+- (void)addLongPressGestureRecognizer:(UIView *)view
+{
+    // 長押し
+    UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPressGesture:)];
+    [self.allGesturesView addGestureRecognizer:longPressGesture];
+    [longPressGesture release];
+}
+
+#pragma mark -
 #pragma mark touch handler
-/* 各種ジェスチャーのハンドラ */
+
 // 一本指でシングルタップ
 - (void)handleSingleFingerSingleTap:(UITapGestureRecognizer *)recognizer {
     [showGestureName setText:@"UITapGestureRecognizer"];
@@ -192,17 +211,17 @@
     [showGestureName setText:@"UISwipeGestureRecognizer"];
     NSString *directionName = nil;
     switch ([recognizer direction]) {
-        case UISwipeGestureRecognizerDirectionUp:
-            directionName = @"UISwipeGestureRecognizerDirectionUp";
-            break;
         case UISwipeGestureRecognizerDirectionRight:
             directionName = @"UISwipeGestureRecognizerDirectionRight";
             break;
-        case UISwipeGestureRecognizerDirectionDown:
-            directionName = @"UISwipeGestureRecognizerDirectionDown";
-            break;
         case UISwipeGestureRecognizerDirectionLeft:
             directionName = @"UISwipeGestureRecognizerDirectionLeft";
+            break;
+        case UISwipeGestureRecognizerDirectionUp:
+            directionName = @"UISwipeGestureRecognizerDirectionUp";        
+            break;
+        case UISwipeGestureRecognizerDirectionDown:
+            directionName = @"UISwipeGestureRecognizerDirectionDown";
             break;
     }
     [showSubInformation setText:[NSString stringWithFormat:@"[%@]", directionName]];
