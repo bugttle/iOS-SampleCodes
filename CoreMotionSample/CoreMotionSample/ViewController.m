477e8cf3b3a468db69c67efb98c1dc279dc1fdc9
UQ Times <uqtimes@gmail.com>
2012-05-16 15:59:58 +0900

    Core Motionのサンプル。プロジェクトの作成直後。

---
+++ CoreMotionSample/CoreMotionSample/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  CoreMotionSample
+//
+//  Created by UQ Times on 12/05/16.
+//  Copyright (c) 2012年 Individual. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
