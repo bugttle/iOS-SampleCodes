8a211d8a15be89c1c809eb42337eb4586ca98567
UQ Times <uqtimes@gmail.com>
2012-05-23 15:52:22 +0900

    CoreLocationのサンプル。プロジェクト作成直後。

---
+++ CoreLocationSample-CurrentLocation/CoreLocationSample-CurrentLocation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreLocationSample-CurrentLocation
+//
+//  Created by UQ Times on 12/05/23.
+//  Copyright (c) 2012年 Individual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
