d29d17489f14498563b34db5aa949382db0cf9a0
UQ Times <uqtimes@gmail.com>
2012-05-25 13:42:49 +0900

    CoreLocationのヘディングサンプル。プロジェクト作成直後。

---
+++ CoreLocationSample-HeadingRelated/CoreLocationSample-HeadingRelated/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreLocationSample-HeadingRelated
+//
+//  Created by UQ Times on 12/05/25.
+//  Copyright (c) 2012年 Individual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CoreLocationSample-HeadingRelated/CoreLocationSample-HeadingRelated/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreLocationSample-HeadingRelated
+//
+//  Created by UQ Times on 12/05/25.
+//  Copyright (c) 2012年 Individual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
