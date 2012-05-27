0c217c51cb93c0451feaf5d42afbe24736f546ba
UQ Times <uqtimes@gmail.com>
2012-05-27 13:19:17 +0900

    CoreLocationのジオコーディングサンプル。プロジェクト作成直後。

---
+++ CoreLocationSample-Geocoding/CoreLocationSample-Geocoding/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoreLocationSample-Geocoding
+//
+//  Created by UQ Times on 12/05/27.
+//  Copyright (c) 2012年 Individual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
